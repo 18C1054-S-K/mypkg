94478c58a27691b0c94cad7ecc2772b1fbbb73b5
18C1054_S_K <s18c1054uc@s.chibakoudai.jp>
2022-12-13 18:17:28 +0900

    add sqsum_test.bash

---
+++ test/sqsum_test.bash
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+dir=~
+[ "$1" != "" ] && dir="$1"
+
+cd $dir/ros2_ws
+colcon build
+
+[ $? -eq 0 ] || exit 1
+
+source $dir/.bashrc
+timeout 20 ros2 launch mypkg sqsum.launch.py > /tmp/mypkg_sqsum.log
+
+cat /tmp/mypkg_sqsum.log | grep "0 = 0^2 + 0^2"
+[ $? -eq 0 ] || exit 2
+cat /tmp/mypkg_sqsum.log | grep "2 = 1^2 + 1^2"
+[ $? -eq 0 ] || exit 3
+cat /tmp/mypkg_sqsum.log | grep "3 can't be sum of 2 square numbers"
+[ $? -eq 0 ] || exit 4
+cat /tmp/mypkg_sqsum.log | grep "4 = 2^2 + 0^2"
+[ $? -eq 0 ] || exit 5
+cat /tmp/mypkg_sqsum.log | grep "5 = 2^2 + 1^2"
+[ $? -eq 0 ] || exit 6
+cat /tmp/mypkg_sqsum.log | grep "9 = 3^2 + 0^2"
+[ $? -eq 0 ] || exit 10
